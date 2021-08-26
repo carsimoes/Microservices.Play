e463537429c46c947586fd2bb6927f9147453bb3
Carlos Renato Simões <carlos.renato.simoes@gmail.com>
2021-08-26 13:59:38 -0300

    Handling errors on Catalog

--- Play.Catalog/src/Play.Catalog.Service/Controllers/ItemsController.cs
+++ Play.Catalog/src/Play.Catalog.Service/Controllers/ItemsController.cs
@@ -25,9 +25,15 @@ namespace Play.Catalog.Service.Controllers
 
         // GET /items/{id}
         [HttpGet("{id}")]
-        public ItemDto GetById(Guid id)
+        public ActionResult<ItemDto> GetById(Guid id)
         {
             var item = items.Where(item => item.Id == id).SingleOrDefault();
+
+            if (item == null)
+            {
+                return NotFound();
+            }
+
             return item;
         }
 
@@ -47,6 +53,11 @@ namespace Play.Catalog.Service.Controllers
         {
             var existingItem = items.Where(item => item.Id == id).SingleOrDefault();
 
+            if (existingItem == null)
+            {
+                return NotFound();
+            }
+
             var updatedItem = existingItem with
             {
                 Name = updateItemDto.Name,
@@ -65,6 +76,12 @@ namespace Play.Catalog.Service.Controllers
         public IActionResult Delete(Guid id)
         {
             var index = items.FindIndex(existingItem => existingItem.Id == id);
+
+            if (index < 0)
+            {
+                return NotFound();
+            }
+
             items.RemoveAt(index);
 
             return NoContent();
