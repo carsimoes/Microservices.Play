31c118112e6b0cbd6bd49db5b00b7f8ca0c90715
Carlos Renato Simões <carlos.renato.simoes@gmail.com>
2021-09-03 17:15:34 -0300

    refactoring

---
+++ Play.Common/src/Play.Common/MassTransit/Extensions.cs
@@ -0,0 +1,33 @@
+using System.Reflection;
+using MassTransit;
+using MassTransit.Definition;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Play.Common.Settings;
+
+namespace Play.Common.MassTransit
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddMassTransitWithRabbitMq(this IServiceCollection services)
+        {
+            services.AddMassTransit(configure =>
+            {
+                configure.AddConsumers(Assembly.GetEntryAssembly());
+
+                configure.UsingRabbitMq((context, configurator) =>
+                {
+                    var configuration = context.GetService<IConfiguration>();
+                    var serviceSettings = configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
+                    var rabbitMQSettings = configuration.GetSection(nameof(RabbitMQSettings)).Get<RabbitMQSettings>();
+                    configurator.Host(rabbitMQSettings.Host);
+                    configurator.ConfigureEndpoints(context, new KebabCaseEndpointNameFormatter(serviceSettings.ServiceName, false));
+                });
+            });
+
+            services.AddMassTransitHostedService();
+
+            return services;
+        }
+    }
+}
